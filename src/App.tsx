32e7224f6b0d2244c700d8acfaa90c136521c3ed
manh <manhdv@amaisoft.com>
2023-04-19 17:05:21 +0700

    update lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '../../../../../../vite.svg'
-import './App.css'
 import { useState } from 'react'
+
+import viteLogo from '../../../../../../vite.svg'
+
+import reactLogo from './assets/react.svg'
+
+import './App.css'
 function App () {
   const [count, setCount] = useState(0)
 

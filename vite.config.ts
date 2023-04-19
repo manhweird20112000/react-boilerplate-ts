a11c3109f969ab4125fa3c437a05bce7ec0dcd99
manh <manhdv@amaisoft.com>
2023-04-19 22:07:49 +0700

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
 });

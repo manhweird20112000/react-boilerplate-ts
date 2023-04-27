55d129ede701fb3d4b64720fa46c55f6b2695678
manh <manhdv@amaisoft.com>
2023-04-27 23:25:00 +0700

    feat: update

--- src/pages/auth/sign-in/index.tsx
+++ src/pages/auth/sign-in/index.tsx
@@ -22,7 +22,7 @@ export function SignIn () {
   return (
     <Form
       form={form}
-      className='bg-red-500'
+      className='bg-red-500 text-red-500'
     >
       <Form.Item name='email'
         label='Email'

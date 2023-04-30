d69bfea2d311a11ea3487867da02b122b65c2365
manh <manhdv@amaisoft.com>
2023-04-30 16:13:57 +0700

    feat: update sign in

--- src/pages/auth/sign-in/index.tsx
+++ src/pages/auth/sign-in/index.tsx
@@ -1,23 +1,33 @@
 import { useMemo } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Button, Form, Space, Typography } from 'antd'
 
 import { TextField } from '@/components/common'
 import { type FormRules } from '@/constants'
+import { setStorage } from '@/utils'
 
 function SignIn () {
   const [form] = Form.useForm()
+  const navigate = useNavigate()
 
   const rules: FormRules = useMemo(() => {
     return {
       email: [
-        { required: true, message: 'ok' },
-        { type: 'email', message: 'ok' }
+        { required: true },
+        { type: 'email' }
       ],
       password: [
-
+        { required: true },
+        { min: 6 }
       ]
     }
   }, [])
+
+
+  const handleSubmit = (event: any) => {
+    setStorage('access_token', 'true')
+    navigate('/')
+  }
 
 
   return (
@@ -25,6 +35,7 @@ function SignIn () {
       <div className={`mx-auto w-[300px]`}>
         <Form
           form={form}
+          onFinish={handleSubmit}
           autoComplete='off'
         >
           <TextField
@@ -43,6 +54,7 @@ function SignIn () {
           />
           <Form.Item>
             <Button
+              onClick={handleSubmit}
               className='w-full'
               size='large'
               type='primary'>Sign In</Button>
@@ -51,11 +63,11 @@ function SignIn () {
         <div className='flex flex-col items-center justify-center'>
           <Space>
             <Typography.Link>
-              Forgot your password?
+              <span className='font-medium text-gray-400'>Forgot your password?</span>
             </Typography.Link>
           </Space>
 
-          <p className='my-4'>
+          <p className='my-4 font-medium text-gray-500'>
             {"Don't have column account?"}
           </p>
 

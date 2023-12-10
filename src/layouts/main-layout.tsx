50ddd2e9cc423639c3bb0253ad559d60da981cbe
h$.weird <dmanh20112000@gmail.com>
2023-12-10 21:03:03 +0700

    feat: update

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,26 +1,9 @@
 import { Outlet } from 'react-router-dom'
-import classNames from 'classnames'
-
-import { Navbar, Sidebar } from '@/components'
-import { useAppSelector } from '@/hooks/redux-hook'
 
 export function MainLayout () {
-  const { isMenuCollapse } = useAppSelector((state) => state.app)
   return (
     <div className='app-container relative'>
-      <Sidebar />
-      <div className={
-        classNames('float-right transition-all duration-500', {
-          'w-[calc(100%_-_240px)]': !isMenuCollapse
-        },
-        {
-          'w-[calc(100%_-_80px)]': isMenuCollapse
-        }
-        )
-      }>
-        <Navbar />
-        <Outlet />
-      </div>
+     <Outlet />
     </div>
   )
 }

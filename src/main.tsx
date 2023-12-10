5497d54b9adfe02fccda9c5ae09b531db30e6cae
h$.weird <dmanh20112000@gmail.com>
2023-12-10 21:53:57 +0700

    feat: update sentry handle error

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { I18nextProvider } from 'react-i18next'
 import { Provider } from 'react-redux'
+import * as Sentry from "@sentry/react"
 import { ConfigProvider } from 'antd'
 
 import App from '@/App'
@@ -12,6 +13,22 @@ import { theme } from '@/themes'
 import 'antd/dist/reset.css'
 // eslint-disable-next-line import/extensions
 import "@/assets/styles/index.scss"
+
+Sentry.init({
+  dsn: `https://${process?.env?.SENTRY_PUBLIC_KEY ?? ''}.ingest.sentry.io/4506371955359744`,
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/]
+    }),
+    new Sentry.Replay()
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+  // Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Suspense>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from "@vitejs/plugin-react-swc";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import tailwindcss from 'tailwindcss'
 import dotenv from 'dotenv'
 import { resolve } from 'path'
@@ -14,6 +15,11 @@ export default ({ mode }) => {
     plugins: [
       react(),
       tsconfigPaths(),
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "weird-1k",
+        project: "javascript-react",
+      }),
     ],
     build: {
       minify: false,
